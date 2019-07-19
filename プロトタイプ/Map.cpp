846538bd1aa7f87b503f60f0974e819dd8033a2c
tajima <tajima.a.work@gmail.com>
2019-07-19 17:26:18 +0900

    [update]07/19進捗とりあえずあげ
    コンストラクタはたぶん完成

--- プロトタイプ/Map.cpp
+++ プロトタイプ/Map.cpp
@@ -1,7 +1,14 @@
 #include "Map.h"
 
-cMap::cMap() {
-	;
+//コンストラクタ、初期化
+cMap::cMap(double _x, double _y, double _w, double _h, int _maxUnit, int _roomNum, vector<sDoor>& _door) {
+	m_x = _x;
+	m_y = _y;
+	m_width = _w;
+	m_height = _h;
+	m_maxUnit = _maxUnit;
+	m_roomNum = _roomNum;
+	m_door = _door;
 }
 
 cMap::~cMap() {
@@ -17,7 +24,8 @@ void cMap::Update() {
 }
 
 void cMap::Draw() {
-	;
+	//とりあえず箱
+	DrawBox(m_x, m_y, m_x + m_width + 1, m_y + m_height + 1, GetColor(255, 255, 255), FALSE);
 }
 
 void cMap::End() {
--- プロトタイプ/Map.h
+++ プロトタイプ/Map.h
@@ -1,35 +1,35 @@
 #pragma once
 #ifndef _MAP_INCLUDE_
 #define _MAP_INCLUDE_
-#include <vector>
-#include "BaseTask.h"
 using namespace std;
+#include <vector>
+#include "DxLib.h"
+#include "BaseTask.h"
 
 class cMap:public cBaseTask {
 private:
-	int m_maxUnit;
+	double m_x, m_y;
 	double m_width, m_height;
+	int m_maxUnit;
 	int m_roomNum;
-	vector<sDoor> door;
-	sPos pos;
+	vector<sDoor> m_door;
 
 public:
-	cMap();
+	cMap(double _x, double _y, double _w, double _h, int _maxUnit, int _roomNum, vector<sDoor>& _door);
 	~cMap();
 	void Init();
 	void Update();
 	void Draw();
 	void End();
+
+	//ドアのアドレス系
+	sDoor* GetDestination(int i) { return m_door[i].destination; }
+	void SetDestination(int i, sDoor* _des) { m_door[i].destination = _des; }
 };
 
 struct sDoor {
 	sDoor* destination;
 	double x, y, width, height;
 };
-
-struct sPos {
-	double x, y;
-};
-
 
 #endif _MAP_INCLUDE_
