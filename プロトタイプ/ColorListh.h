37f533e8e9a5b22081531ed64ac1d4484b54cd0f
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-07-26 21:08:44 +0900

    [update]色の追加と二重インクルード防止
    黄色とピンクの追加
    ifndef文の追加

--- プロトタイプ/ColorListh.h
+++ プロトタイプ/ColorListh.h
@@ -1,17 +1,26 @@
 #pragma once
 #include "DxLib.h"
+
+#ifndef _INCLUED_COLORLIST_
+#define _INCLUED_COLORLIST_
+
 /********************************************************
 ●概要
 　Dxライブラリのカラー関数の入力を簡易化することで
   デバッグを楽にすることを想定しています。
+  また、色で誰のデバッグ内容か判別。
 ********************************************************/
 
 /********************************************************
 RGB色 一覧
 命名規則：色の略語2文字
 ********************************************************/
-#define WH	GetColor(255,255,255)	// 白
-#define BK	GetColor(0,0,0)			// 黒
-#define RD	GetColor(255,0,0)		// 赤
-#define GR	GetColor(0,255,0)		// 緑
-#define BL	GetColor(0,0,255)		// 青
+#define WH	GetColor(255,255,255)	// 白	システム系
+#define BK	GetColor(0,0,0)			// 黒	
+#define RD	GetColor(255,0,0)		// 赤	高井
+#define GR	GetColor(0,255,0)		// 緑	益子
+#define BL	GetColor(0,0,255)		// 青	竹内
+#define YL	GetColor(255,255,0)		// 黄	石倉
+#define PK	GetColor(234,145,152)	// 桃	田島
+
+#endif // !_INCLUED_COLORLIST_
