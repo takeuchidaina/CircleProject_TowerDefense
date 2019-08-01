1cff4b458b15e91b6362ed975a6f2cc55eb6a180
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-08-01 13:03:36 +0900

    [add]シーンを管理するクラスの基底クラスBaseSceneの作成
    SceneMgrとGameMgrが継承予定

---
+++ プロトタイプ/BaseScene.cpp
@@ -0,0 +1,17 @@
+#include "BaseScene.h"
+
+cBaseScene::cBaseScene(ISceneChanger* _scene){
+	sceneChanger = _scene;
+}
+
+void cBaseScene::Update() {
+
+}
+
+void cBaseScene::Draw() {
+
+}
+
+void cBaseScene::End() {
+
+}
