fa2d9485b6bf27337cb1e3b12971d5971ec493f1
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-08-30 15:26:01 +0900

    [update]タイトルから項目を選択してエンターでシーン移動ができるように変更
    オブジェクトの生成とデリートを二回行っているエラーがあり、止まっています

--- プロトタイプ/Title.h
+++ プロトタイプ/Title.h
@@ -24,24 +24,42 @@ public:
 
 private:
 
+#ifndef TITLE_DEBUG
 	// メニュー項目一覧
 	typedef enum {
-		E_MENU_GAME,
-		E_MENU_END,
-		
-	}eMenu;
+		E_TITLE_GAME,
+		E_TITLE_END,
+		E_TITLE_MAX
+	}eTitle;
+#endif // !TITLE_DEBUG
+
+#ifdef TITLE_DEBUG
+	// メニュー項目一覧
+	typedef enum {
+		E_TITLE_MENU,
+		E_TITLE_GAME,
+		E_TITLE_RESULT,
+		E_TITLE_END,
+		E_TITLE_MAX
+	}eTitle;
+#endif // TITLE_DEBUG
+
+
 
 	// メニューの必要情報
 	typedef struct {
 		int x, y;
-		eMenu menu;
+		eTitle menu;
 		char text[20];
 	}sSelectMenu;
 
-	sSelectMenu m_menu[2];
+	const int menuNum = E_TITLE_MAX;
+	sSelectMenu m_menu[E_TITLE_MAX];
 
-	static const int fontSize = 30;
+	const int FONT_SIZE = 20;
+	const int TEXT_X = 350;
+	const int TEXT_Y = 300;
 	int nowSelect;
-	const int menuNum = 2;
+
 
 };
