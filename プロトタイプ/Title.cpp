b7c57ce61f0d835e2878f5d19d17fbc38842f59e
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-08-30 11:42:51 +0900

    [update]メニュー選択の作成(未デバッグ-エラー対処の為一時保存)
    ソース外でのエラーが出たため一時退避

--- プロトタイプ/Title.cpp
+++ プロトタイプ/Title.cpp
@@ -5,9 +5,24 @@ cTitle::cTitle(ISceneChanger* _scene) : cBaseScene(_scene) {
 }
 
 void cTitle::Init() {
+
+	// x,y,eMenu,name
+	m_menu[E_MENU_GAME] = { 300,300,E_MENU_GAME,"ゲーム開始" },
+	m_menu[E_MENU_END] = { 300,300,E_MENU_END,"ゲーム終了"};
+
+	nowSelect = E_MENU_GAME;
 }
 
 void cTitle::Update() {
+	
+	if (GET_KEY_PRESS(KEY_INPUT_DOWN) == 1) {
+		nowSelect++;
+
+		//if (nowSelect > menuNum) {
+		//	nowSelect = 0;
+		//}
+	}
+
 
 #ifdef TITLE_DEBUG
 
@@ -36,8 +51,16 @@ void cTitle::Draw() {
 
 	cBaseScene::Draw();
 
+	for (int i = 0; i < menuNum; i++) {
+		//DrawFormatString(m_menu[i].x,m_menu[i].y,WH,m_menu[i].text);
+		//DrawFormatString(m_menu[nowSelect].x - 20, m_menu[nowSelect].y, WH, "→");
+	}
+	
+	
+
 #ifdef TITLE_DEBUG
 	DrawFormatString(0, 0, WH, "タイトル画面");
+	DrawFormatString(0, 20, WH, "nowSelect:%d",nowSelect);
 #endif // TITLE_DEBUG
 
 }
--- プロトタイプ/Title.h
+++ プロトタイプ/Title.h
@@ -8,6 +8,9 @@
 #include "ISceneChanger.h"
 #include "Keyboard.h"
 #include "Mouse.h"
+#include <string.h>
+
+using namespace std;         //  名前空間指定
 
 class cTitle : public cBaseScene {
 
@@ -20,5 +23,25 @@ public:
 	void End()override;
 
 private:
+
+	// メニュー項目一覧
+	typedef enum {
+		E_MENU_GAME,
+		E_MENU_END,
+		
+	}eMenu;
+
+	// メニューの必要情報
+	typedef struct {
+		int x, y;
+		eMenu menu;
+		char text[20];
+	}sSelectMenu;
+
+	sSelectMenu m_menu[2];
+
+	static const int fontSize = 30;
+	int nowSelect;
+	const int menuNum = 2;
 
 };
