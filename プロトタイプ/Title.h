15a19997c956f0a0ac84ed8488373faadf7d596a
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-10-04 20:51:23 +0900

    [update]開始ボタン,終了ボタンの表示
    ボタンで遷移はしません

--- プロトタイプ/Title.h
+++ プロトタイプ/Title.h
@@ -24,15 +24,6 @@ public:
 
 private:
 
-#ifndef TITLE_DEBUG
-	// メニュー項目一覧
-	typedef enum {
-		E_TITLE_GAME,
-		E_TITLE_END,
-		E_TITLE_MAX
-	}eTitle;
-#endif // !TITLE_DEBUG
-
 #ifdef TITLE_DEBUG
 	// メニュー項目一覧
 	typedef enum {
@@ -44,8 +35,6 @@ private:
 	}eTitle;
 #endif // TITLE_DEBUG
 
-
-
 	// メニューの必要情報
 	typedef struct {
 		int x, y;
@@ -53,13 +42,17 @@ private:
 		char text[20];
 	}sSelectMenu;
 
-	const int m_menuNum = E_TITLE_MAX;
-	sSelectMenu m_menu[E_TITLE_MAX];
+	const int m_menuNum = E_TITLE_MAX;	//メニュー項目数
+	sSelectMenu m_menu[E_TITLE_MAX];	//メニュー情報の実態宣言
 
-	const int FONT_SIZE = 20;
-	const int TEXT_X = 350;
-	const int TEXT_Y = 300;
-	int nowSelect = 0;
+	const int FONT_SIZE = 20;	//フォントサイズ
+	const int TEXT_X = 350;		//一行目の座標x
+	const int TEXT_Y = 300;		//一行目の座標y
+	int nowSelect = 0;			//現在の選択メニュー
 
+	int m_background;			//背景画像
+	int m_logo;					//ロゴ画像
+	int m_startButton;			//開始ボタン
+	int m_endButton;			//終了ボタン
 
 };
