4fd611a1379eccf6b310f6d4387f01c01ac16b24
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-11-22 19:56:37 +0900

    [fix]WinBoxで出ていたエラーを解決
    文字コードの問題かと思われます

--- プロトタイプ/WinBox.cpp
+++ プロトタイプ/WinBox.cpp
@@ -2,17 +2,15 @@
 #include "WinBox.h"
 
 void ErrBox(const char* tmp) {
-	MessageBox(
-		NULL,
+	MessageBox(NULL,
 		TEXT(tmp),
-		TEXT("エラー"),
-		MB_OK | MB_ICONWARNING);
+		TEXT("ERROR"), 
+		MB_OK | MB_ICONINFORMATION);
 }
 
 void ReportBox(const char* tmp) {
-	MessageBox(
-		NULL,
+	MessageBox(NULL,
 		TEXT(tmp),
-		TEXT("報告"),
-		MB_OK | MB_ICONINFORMATION);
+		TEXT("REPORT"),
+		MB_OK);
 }
