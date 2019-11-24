26347da0e20a416bf1037ade91ab83682ea81302
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-11-24 12:00:52 +0900

    [update]音を鳴らす基盤の作成
    配列にしていきます

--- プロトタイプ/Sound.cpp
+++ プロトタイプ/Sound.cpp
@@ -11,11 +11,22 @@ cSound::~cSound()
 }
 
 void cSound::Init() {
+	m_SEPath[0] = { "../resource/Sound/SE/test.wav"};
+	m_SEList[0] = LoadSoundMem(m_SEPath[0].c_str());
+	FileCheck(m_SEList[0]);
 
+	m_BGMPath[0] = { "../resource/Sound/BGM/ÉGÉäÉJ.wav" };
+	m_BGMList[0] = LoadSoundMem(m_BGMPath[0].c_str());
+	FileCheck(m_BGMList[0]);
 }
 
 void cSound::Update() {
-
+	if (GET_KEY_PRESS(KEY_INPUT_S) == 1) {
+		PlaySoundMem(m_SEList[0], DX_PLAYTYPE_NORMAL,FALSE);
+	}
+	if (GET_KEY_PRESS(KEY_INPUT_G) == 1) {
+		PlaySoundMem(m_BGMList[0], DX_PLAYTYPE_BACK,FALSE);
+	}
 }
 
 void cSound::Draw() {
--- プロトタイプ/Sound.h
+++ プロトタイプ/Sound.h
@@ -2,8 +2,13 @@
 #ifndef _INCLUED_SOUND_
 #define _INCLUED_SOUND_
 #include "DxLib.h"
+#include <iostream>
 #include "BaseTask.h"
 #include "ErrorCheck.h"
+#include "Keyboard.h"
+#include "string.h"
+
+using namespace std;
 
 class cSound : cBaseTask
 {
@@ -16,7 +21,27 @@ public:
 	virtual void Draw();
 	virtual void End();
 
+	typedef enum{
+		E_SELECT,
+		E_CANSEL,
+		E_BLADE,
+		E_SE_LENGTH,
+	}SE;
+
+	typedef enum{
+		E_SEA,
+		E_TITLE,
+		E_BATTLE,
+		E_WIN,
+		E_LOSE,
+		E_BGM_LENGTH
+	}BGM;
+
 private:
+	string m_SEPath[E_SE_LENGTH];
+	string m_BGMPath[E_BGM_LENGTH];
+	int m_SEList[E_SE_LENGTH];
+	int m_BGMList[E_BGM_LENGTH];
 
 };
 
