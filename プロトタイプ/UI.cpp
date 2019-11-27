5bb1d147e7d81bc0a8a4a533b53a0518b75d40ac
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-11-28 00:57:07 +0900

    [add]UIの骨組み追加
    basetaskの最低限必要なものを記述

---
+++ プロトタイプ/UI.cpp
@@ -0,0 +1,31 @@
+#include "UI.h"
+
+cUI::cUI()
+{
+
+}
+
+cUI::~cUI()
+{
+
+}
+
+void cUI::Init() {
+
+
+}
+
+void cUI::Update() {
+
+
+}
+
+void cUI::Draw() {
+
+
+}
+
+void cUI::End() {
+
+
+}
---
+++ プロトタイプ/UI.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef _INCLUED_UI_
+#define _INCLUED_UI_
+
+#include "DxLib.h"
+#include "BaseTask.h"
+
+class cUI : public cBaseTask
+{
+public:
+	cUI();
+	~cUI();
+
+private:
+
+};
+
+
+
+#endif // !_INCLUED_UI_
+
