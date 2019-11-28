f4b1e823e9a1ab6ee56f99a3dc37a34d2e8d064b
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-11-29 04:56:26 +0900

    [update]キャラクターとUI用の背景を配置
    数は取得していないです

--- プロトタイプ/UI.cpp
+++ プロトタイプ/UI.cpp
@@ -2,30 +2,60 @@
 
 cUI::cUI()
 {
-
+	Init();
 }
 
 cUI::~cUI()
 {
-
+	End();
 }
 
 void cUI::Init() {
 
+	int bgSizeX = 130;
+	int bgSizeY = 42;
+	int charSize = 32;
+	m_UI[E_CHAR_SORD]   = { 10, 10, 62, 42,"../resource/img/UI_Sord.png",0 };
+	m_UI[E_CHAR_ARCHER] = { 10, 52, 62, 84,"../resource/img/UI_Archer.png",0 };
+	m_UI[E_CHAR_SHIELD] = { 10, 94, 62, 126,"../resource/img/UI_Shield.png",0 };
+	
+	int tyousei = 5;
+	m_UI[E_BG_SORD]   = { m_UI[E_CHAR_SORD].ux - tyousei, m_UI[E_CHAR_SORD].uy - tyousei,
+						  m_UI[E_CHAR_SORD].ux - tyousei + bgSizeX, m_UI[E_CHAR_SORD].uy - tyousei + bgSizeY,
+							"../resource/img/UI_BG.png",0 };
+	m_UI[E_BG_ARCHER] = { m_UI[E_CHAR_ARCHER].ux - tyousei, m_UI[E_CHAR_ARCHER].uy - tyousei,
+						  m_UI[E_CHAR_ARCHER].ux - tyousei + bgSizeX, m_UI[E_CHAR_ARCHER].uy - tyousei + bgSizeY
+							,"../resource/img/UI_BG.png",0 };
+	m_UI[E_BG_SHIELD] = { m_UI[E_CHAR_SHIELD].ux - tyousei, m_UI[E_CHAR_SHIELD].uy - tyousei,
+						  m_UI[E_CHAR_SHIELD].ux - tyousei + bgSizeX, m_UI[E_CHAR_SHIELD].uy - tyousei + bgSizeY,
+							"../resource/img/UI_BG.png",0 };
 
+	m_UI[E_BG_SUM] = { m_UI[E_BG_SHIELD].ux, m_UI[E_BG_SHIELD].uy + bgSizeY,
+						  m_UI[E_BG_SHIELD].ux + bgSizeX / 2, m_UI[E_CHAR_SHIELD].uy + bgSizeY + bgSizeY - tyousei,
+						"../resource/img/UI_BG.png",0 };
+
+	for (int i = 0; i < E_UI_LENGTH;i++) {
+		m_UI[i].image = LoadGraph(m_UI[i].path.c_str());
+		FileCheck(m_UI[i].image);
+	}
 }
 
 void cUI::Update() {
-
+	//キャラクターの数を取得
 
 }
 
 void cUI::Draw() {
 
+	for (int i = 0; i < E_UI_LENGTH; i++) {
+		DrawExtendGraph(m_UI[i].ux, m_UI[i].uy, m_UI[i].dx, m_UI[i].dy, m_UI[i].image, TRUE);
+	}
 
 }
 
 void cUI::End() {
-
-
+	for (int i = 0; i < E_UI_LENGTH; i++) {
+		DeleteGraph(m_UI[i].image);
+	}
+	
 }
--- プロトタイプ/UI.h
+++ プロトタイプ/UI.h
@@ -4,19 +4,46 @@
 #define _INCLUED_UI_
 
 #include "DxLib.h"
+#include <iostream>
 #include "BaseTask.h"
+#include "string.h"
+#include "ErrorCheck.h"
+
+using namespace std;
 
 class cUI : public cBaseTask
 {
 public:
 	cUI();
 	virtual ~cUI();
+
 	virtual void Init();
 	virtual void Update();
 	virtual void Draw();
 	virtual void End();
 
 private:
+
+	typedef enum {
+		E_BG_SORD,
+		E_BG_ARCHER,
+		E_BG_SHIELD,
+		E_BG_SUM,
+		E_CHAR_SORD,
+		E_CHAR_ARCHER,
+		E_CHAR_SHIELD,
+		E_UI_LENGTH
+	}eUI;
+
+	typedef struct {
+		float ux, uy,dx,dy;
+		string path;
+		int image;
+	}sUI;
+
+	sUI m_UI[E_UI_LENGTH];
+	int playerCnt[3];
+	int maxPlayer;
 
 };
 
