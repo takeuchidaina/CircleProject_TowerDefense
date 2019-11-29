0ea54b2683fdac3940f3b6910910a4e59df0fd55
竹内 <41311080+takeuchidaina@users.noreply.github.com>
2019-11-29 23:48:11 +0900

    [fix]GM.hの競合を解決

--- プロトタイプ/GameMgr.h
+++ プロトタイプ/GameMgr.h
@@ -23,6 +23,9 @@
 #ifndef _INCLUDE_GAMEMGR_
 #define _INCLUDE_GAMEMGR_
 
+#define SPAWN_CNT 400
+#define MOVE_CNT 100
+
 class cGameMgr : public cBaseScene{
 
 public:
@@ -59,6 +62,10 @@ private:
 	sBG m_Cloud[2];
 	const float CLOUD_SPEED = 0.25f;
 	const int TIME_LIMIT = 120;
+	int SpawnCnt = 0;			// 一定数まで行ったらスポーン
+	int SpawnType = 0;			// スポーンするタイプを決めるランダム
+	int MoveCnt = 0;			// 一定数まで行ったら移動
+	int MoveType = 0;			// moveするTypeを決めるランダム
 
 	void MoveBackGround();
 };
