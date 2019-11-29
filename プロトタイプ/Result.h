c7494ce4c311930a834a6183165bbd80b0e4b84e
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-11-30 05:07:50 +0900

    [upate]リザルト画面からタイトル画面に戻れるように変更
    マウス操作です

--- プロトタイプ/Result.h
+++ プロトタイプ/Result.h
@@ -8,6 +8,12 @@
 #include "ISceneChanger.h"
 #include "Keyboard.h"
 #include "Mouse.h"
+#include "Button.h"
+#include <string.h>
+#include "ErrorCheck.h"
+#include "Sound.h"
+
+using namespace std;         //  名前空間指定
 
 class cResult : public cBaseScene {
 
@@ -20,5 +26,15 @@ public:
 	void End()override;
 
 private:
-	int m_image;
+	int m_BG;
+
+	// 画像配置の必要情報
+	typedef struct {
+		double ux = 0, uy = 0, dx = 0, dy = 0;	//左上xy,右下xy
+		string filePath = "";		//画像のファイルパス
+		int handle = 0;				//画像ハンドル
+	}sImage;
+
+	sImage m_image;
+	cButton m_btn;
 };
