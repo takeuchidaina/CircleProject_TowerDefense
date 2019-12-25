fc7d53f6b42dda791261e0c3c833c4d5ab7bec5f
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-12-26 01:38:52 +0900

    [update]船の反転、それに伴う雲の移動の反転
    雲の移動速度は変わらず

--- src/InGameBackGroundcpp.cpp
+++ src/InGameBackGroundcpp.cpp
@@ -24,7 +24,7 @@ void cBackGround::Init() {
 	m_cloud[0].pos = VGet(0.0f, 0.0f, 0.0f);
 	m_cloud[0].image = LoadGraph("../resource/img/BG_Cloud.png");
 	FileCheck(m_cloud[0].image);
-	m_cloud[1].pos = VGet(-1870.0f, 0.0f, 0.0f);
+	m_cloud[1].pos = VGet(1870.0f, 0.0f, 0.0f);
 	m_cloud[1].image = LoadGraph("../resource/img/BG_Cloud.png");
 	FileCheck(m_cloud[1].image);
 }
@@ -57,16 +57,16 @@ void cBackGround::End() {
 void cBackGround::MoveBackGround() {
 
 	//移動
-	m_cloud[0].pos.x += CLOUD_SPEED;
-	m_cloud[1].pos.x += CLOUD_SPEED;
+	m_cloud[0].pos.x -= CLOUD_SPEED;
+	m_cloud[1].pos.x -= CLOUD_SPEED;
 
 	//画面外処理、スクロール設定
 	//画像が横に繋がるようになっている
 	//画面外に行くと、画面内の画像の後ろに移動
-	if (m_cloud[0].pos.x >= 1870.0f) {
-		m_cloud[0].pos.x = m_cloud[1].pos.x - 1870.0f;
+	if (m_cloud[0].pos.x <= -1870.0f) {
+		m_cloud[0].pos.x = m_cloud[1].pos.x + 1870.0f;
 	}
-	else if (m_cloud[1].pos.x >= 1870.0f) {
-		m_cloud[1].pos.x = m_cloud[0].pos.x - 1870.0f;
+	else if (m_cloud[1].pos.x <= -1870.0f) {
+		m_cloud[1].pos.x = m_cloud[0].pos.x + 1870.0f;
 	}
 }
